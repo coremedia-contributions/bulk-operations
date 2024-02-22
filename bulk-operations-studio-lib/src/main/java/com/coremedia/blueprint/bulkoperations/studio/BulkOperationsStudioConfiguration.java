9d0f4c300f1fa2c8cb44afc337f5ede95a797c69
Sebastian Büttner <sebastian.buettner@coremedia.com>
2024-02-22 08:54:44 +0100

    Refactoring spring.factories to new spring auto configuration approach

--- bulk-operations-studio-lib/src/main/java/com/coremedia/blueprint/bulkoperations/studio/BulkOperationsStudioConfiguration.java
+++ bulk-operations-studio-lib/src/main/java/com/coremedia/blueprint/bulkoperations/studio/BulkOperationsStudioConfiguration.java
@@ -1,14 +1,14 @@
 package com.coremedia.blueprint.bulkoperations.studio;
 
 import com.coremedia.blueprint.bulkoperations.studio.bulk.BulkOperationsResource;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@AutoConfiguration
 public class BulkOperationsStudioConfiguration {
 
   @Bean
-  BulkOperationsResource bulkOperationsResource() {
+  public BulkOperationsResource bulkOperationsResource() {
     return new BulkOperationsResource();
   }
 
