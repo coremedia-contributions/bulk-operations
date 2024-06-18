0782a6b03bec80ceaee1deb17c534e9292ac9a62
David Hoeck <david.hoeck@coremedia.com>
2024-06-18 13:18:31 +0200

    add resourceBundle properties for locale 'ja'

---
+++ apps/studio-client/apps/main/bulk-operations-studio-plugin/src/BulkOperations_ja_properties.ts
@@ -0,0 +1,8 @@
+import ResourceBundleUtil from "@jangaroo/runtime/l10n/ResourceBundleUtil";
+import BulkOperations_properties from "./BulkOperations_properties";
+
+/**
+ * Overrides of ResourceBundle "BulkOperations" for Locale "ja".
+ * @see BulkOperations_properties#INSTANCE
+ */
+ResourceBundleUtil.override(BulkOperations_properties, {});
